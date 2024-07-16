642d19e0922a9979e62e7edf4aa9105e8ebc3da6
Broys42 <boxforanswers@mail.ru>
2024-07-17 00:49:13 +0300

    Added tests commands

--- database/create_db.py
+++ database/create_db.py
@@ -4,5 +4,14 @@ if __name__ == "__main__":
     async def main():
         async_orm = AsyncORM()
         await async_orm.create_tables()
+        await async_orm.insert_headphones(name="Кукусики", price=123, description="ЫВЫ", stock_quantity=4)
+        await async_orm.insert_headphones(name="ЫФВЫФВ", price=123, description="ВАПАВ", stock_quantity=5)
+        # await async_orm.delete_headphones(headphones_id=1)
+        # await async_orm.select_all_shopitem()
+        await async_orm.create_user(username="Шпеп", email="Шпеп", password="Шпе")
+        await async_orm.create_cart(1)
+        await async_orm.add_shopitem_into_cartitems(headphones_id=1, cart_id=1, quantity=3)
+        # await async_orm.select_cartitems(1)
+        await async_orm.delete_headphones(1)
 
     asyncio.run(main())
