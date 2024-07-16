3f5bd9360a2e31cb452caef3b6b2c57ef13164c9
Broys42 <boxforanswers@mail.ru>
2024-07-17 00:48:58 +0300

    Renamed Header class

--- ui/header.py
+++ ui/header.py
@@ -1,7 +1,7 @@
 import flet as ft
 
 
-class Header(ft.Stack):
+class HeaderInMainPage(ft.Stack):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.height = 50
@@ -99,11 +99,83 @@ class Header(ft.Stack):
         self.transparent_switcher.content = self.transparent_background_color if to_transparent else self.not_transparent_background_color
         self.transparent_switcher.update()
 
+class HeaderOutsideMainPage(ft.Stack):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.height = 50
+        self.page = page
+        self.alignment = ft.alignment.center
+
+        self.not_transparent_background_color = ft.Container(
+            bgcolor="#ffffff",
+            padding=0
+        )
+
+        self.go_to_main_page_button = ft.Container(
+            content=ft.Text(
+                value="На главную",
+                weight=ft.FontWeight.BOLD,
+            )
+        )
+
+        self.show_contacts_button = ft.Container(
+            content=ft.Text(
+                value="Контакты",
+                weight=ft.FontWeight.BOLD
+            ),
+        )
+
+        self.logo_text = ft.Container(
+            content=ft.Text(
+                value="BROYS SHOP",
+                weight=ft.FontWeight.BOLD,
+            ),
+            padding=ft.Padding(left=30, right=0, top=0, bottom=0),
+            width=150
+        )
+
+        self.text_buttons_row = ft.Row(
+            controls=[
+                self.go_to_main_page_button,
+                self.show_contacts_button
+            ],
+            spacing=30,
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+
+        #No IconButton to avoid all control animation
+        self.shop_bag_icon = ft.Icon(
+            name="SHOPPING_BAG",
+            color="black",
+            size=20,
+        )
+
+        self.container_for_icon = ft.Container(
+            content=self.shop_bag_icon,
+            alignment=ft.alignment.center_right,
+            padding=ft.Padding(left=0, right=30, top=0, bottom=0),
+            width=150
+        )
+
+        self.header_buttons = ft.Row(
+            controls=[
+                self.logo_text,
+                self.text_buttons_row,
+                self.container_for_icon
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+        )
+
+        self.controls = [
+            self.not_transparent_background_color,
+            self.header_buttons
+        ]
+
 #Test launch (python -m ui.header)
 if __name__ == "__main__":
     def main(page: ft.Page):
         page.padding = 0
-        header = Header(page)
+        header = HeaderInMainPage(page)
         page.add(header)
         page.bgcolor = "black"
         page.update()
