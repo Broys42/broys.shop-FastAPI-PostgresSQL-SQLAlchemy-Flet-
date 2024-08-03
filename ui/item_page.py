b4837481853240c1292017677960a2d3a6ad51ea
Broys42 <boxforanswers@mail.ru>
2024-08-04 00:58:39 +0300

    Added custom headphones_id

--- ui/item_page.py
+++ ui/item_page.py
@@ -1,25 +1,38 @@
+import asyncio
 import flet as ft
 from ui.main_btn import MainButton
 from ui.item_counter import ItemCounter
+from ui.header import HeaderOutsideMainPage
+from ui.footer import Footer
 
-
-class ItemPage(ft.Row):
-    def __init__(self, page: ft.Page):
+class HeadphonesPage(ft.Stack):
+    def __init__(self, page: ft.Page, headphones_id):
         super().__init__()
-        self.height = 500
-        self.add_in_cart_button = MainButton(button_text="Добавить в корзину", width=300)
+        self.page = page
+        self.expand = True
+
+        self.page.padding = 0
+
+        self.header = HeaderOutsideMainPage(self.page)
+        self.footer = Footer(self.page)
+
+        self.headphones_id = headphones_id
+
+        self.add_in_cart_button = MainButton(
+            button_text="Добавить в корзину", width=300)
+
         self.item_counter = ItemCounter()
 
         self.item_image = ft.Image(
             src="https://raw.githubusercontent.com/Broys42/broys.shop/main/assets/images/headphones/a50-gallery-ps4-01-refresh.png",
             width=500,
-            height=500,
-            fit = ft.ImageFit.FIT_HEIGHT
+            height=450,
+            fit=ft.ImageFit.FIT_HEIGHT
         )
 
         self.item_title = ft.Text(
-            value="Item Title",
-            size=50,
+            value=f"{self.headphones_id}",
+            size=40,
             color=ft.colors.WHITE,
             weight=ft.FontWeight.BOLD
         )
@@ -39,15 +52,17 @@ class ItemPage(ft.Row):
         self.item_title_and_divider = ft.Column(
             controls=[
                 self.item_title,
-                ft.Divider(thickness=3, color="white")
-            ]
+                ft.Divider(thickness=1, color="white")
+            ],
+            spacing=0
         )
 
         self.item_counter_and_description = ft.Column(
             controls=[
                 self.item_counter,
                 self.item_description,
-            ]
+            ],
+            spacing=10
         )
 
         self.item_info_pannel = ft.Column(
@@ -57,21 +72,63 @@ class ItemPage(ft.Row):
                 self.add_in_cart_button
             ],
             width=300,
-            alignment=ft.MainAxisAlignment.SPACE_EVENLY
+            height=400,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+        )
+
+        self.item_card = ft.Row(
+            controls=[
+            self.item_image,
+            self.item_info_pannel
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+
+
+        # Divider is save space for header
+        self.page_with_divider = ft.Column(
+            controls=[
+                ft.Divider(height=50),
+                self.item_card,
+            ],
+            expand=True,
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            scroll=ft.ScrollMode.ALWAYS,
+            on_scroll_interval=0,
+        )
+
+        self.page_without_header = ft.Column(
+            controls=[
+                self.page_with_divider,
+                self.footer
+            ],
+            alignment=ft.alignment.center,
         )
 
         self.controls = [
-            self.item_image,
-            self.item_info_pannel
+            self.page_without_header,
+            self.header,
         ]
 
+        self.page.on_resized = self.page_on_resize
 
-#Test launch (python -m ui.item_page)
+    def page_on_resize(self, e: ft.WindowResizeEvent):
+        self.height = e.page.height
+        self.page.update()
+
+    def change_product(self, headphones_id):
+        self.headphones_id=headphones_id
+        print("Updated")
+        self.update()
+
+
+# Test launch (python -m ui.item_page)
 if __name__ == "__main__":
-    def main(page: ft.Page):
-        item_page = ItemPage(page)
+    async def main(page: ft.Page):
+        page.bgcolor = "#e4c4c4"
+        item_page=HeadphonesPage(page=page, headphones_id=123)
+        page.padding = 0
         page.add(item_page)
-        page.bgcolor = "black"
-        page.update()
 
-    ft.app(main)
+    asyncio.run(ft.app_async(main))
